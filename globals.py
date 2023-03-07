bf1c25e12cd63e7574333447ad51b0ae0f350173
ranjsidhu <ranjeetsidhu7433@gmail.com>
2023-03-07 18:03:55 +0000

    add globals.py

---
+++ globals.py
@@ -0,0 +1,11 @@
+class dbinfo :
+    database = 'nfhcw2'
+    localhost = 'DESKTOP-JKLT1OR\SQLEXPRESS' # may need to change
+    server = 'DESKTOP-JKLT1OR\SQLEXPRESS' # may need to change
+    remoteserver = 'DESKTOP-JKLT1OR\SQLEXPRESS' # may need to change
+    username = 'nfhuser'
+    password = 'nfhuserpwd'
+    adminuser = 'nfhtest'
+    adminpwd = 'nfhtest'
+    cnx_nbr = 0
+    loggedin = 0
